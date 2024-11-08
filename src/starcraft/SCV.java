f47b2cc53d40d511ede5e5749f10e6ed101f9321
Khankw <jskw247@gmail.com>
2024-11-08 15:00:34 +0900

    Add SCV repair()

--- src/starcraft/SCV.java
+++ src/starcraft/SCV.java
@@ -6,6 +6,21 @@ public class SCV extends GroundUnit implements Repairable {
 	}
 
 	public void repair(Repairable unit) {
+		if (unit instanceof Unit) {
+			Unit target = (Unit) unit;
 
+			while (target.hp < target.MAX_HP) {
+				String msg = String.format("%s 수리중... [%d/%d]", target.name, target.hp, target.MAX_HP);
+				System.out.println(msg);
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				target.hp++;
+			}
+			System.out.println(target.name + " 수리완료.");
+		}
 	}
 }
