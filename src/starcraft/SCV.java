6d763b3f332eafb98bce723e050c4d71f2105a3c
Khankw <jskw247@gmail.com>
2024-11-08 16:42:21 +0900

    Update repair repairCost

--- src/starcraft/SCV.java
+++ src/starcraft/SCV.java
@@ -7,9 +7,11 @@ public class SCV extends GroundUnit implements Repairable, Attackable {
 
 	public void repair(Repairable unit) {
 		if (unit instanceof Unit) {
+			System.out.println("------------------------------------");
 			Unit target = (Unit) unit;
 
-			while (target.hp < target.MAX_HP) {
+			int repairCost=10;
+			while (target.hp < target.MAX_HP && repairCost > 0) {
 				String msg = String.format("%s 수리중... [%d/%d]", target.name, target.hp, target.MAX_HP);
 				System.out.println(msg);
 
@@ -19,17 +21,20 @@ public class SCV extends GroundUnit implements Repairable, Attackable {
 					e.printStackTrace();
 				}
 				target.hp++;
+				repairCost--;
 			}
-			System.out.println(target.name + " 수리완료.");
+			System.out.println(target.name + " 수리완료.\n");
+			
 		}
 	}
 
 	@Override
 	public void attack(Unit unit) {
+		System.out.println("------------------------------------");
 		if (unit instanceof AirUnit) {
 			AirUnit target = (AirUnit) unit;
 			if (target.isFly) {
-				System.out.println(name + "이 " + target.name + "에게 공격시도 했지만 비행중이어서 실패합니다.");
+				System.out.println(name + "이 " + target.name + "에게 공격시도 했지만 비행중이어서 실패합니다.\n");
 				return;
 			}
 		}
@@ -38,6 +43,6 @@ public class SCV extends GroundUnit implements Repairable, Attackable {
 		unit.hp -= attack;
 		if (unit.hp < 0)
 			unit.hp = 0;
-		System.out.println(unit.name + "의 HP는 " + unit.hp + "입니다.");
+		System.out.println(unit.name + "의 HP는 " + unit.hp + "입니다.\n");
 	}
 }
